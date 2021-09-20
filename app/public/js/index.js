82812f5ef9c3463c5837b7ed07a371e7b1f26c33
rhj0970 <55927863+rhj0970@users.noreply.github.com>
2021-09-20 10:19:49 -0400

    finalize hw4

--- app/public/js/index.js
+++ app/public/js/index.js
@@ -22,21 +22,27 @@ const Offer = {
           ]
       }
   },
-  created() {
-      console.log("A");
-      fetch('https://randomuser.me/api/')
-      .then( response => response.json() )
-      .then( (responseJson) => {
-          console.log(responseJson);
-          console.log("C");
-          this.person = responseJson.results[0];
-      })
-      .catch( (err) => {
-          console.error(err);
-      })
-      console.log("B");
-  } //end created
-} // end Offer config
+  methods: {
+    fetchUserData() {
+        console.log("A");
+        fetch('https://randomuser.me/api/')
+        .then( response => response.json() )
+        .then( (responseJson) => {
+            console.log(responseJson);
+            console.log("C");
+            this.person = responseJson.results[0];
+        })
+        .catch( (err) => {
+            console.error(err);
+        })
+        console.log("B");
+    }
+},
+created() {
+    this.fetchUserData();
+} //end created
+}
 
 Vue.createApp(Offer).mount('.offerApp');
 console.log("Z");
+
